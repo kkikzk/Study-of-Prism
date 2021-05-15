661ebe6379edf82becea38f0a543375b225e0bf1
KAKEI Kazuki <kkikzk@gmail.com>
2021-05-15 17:37:12 +0900

    Menuのプロジェクトを追加

---
+++ StudyOfPrism-Menu/StudyApp.Manu/ManuModule.cs
@@ -0,0 +1,16 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+
+namespace StudyApp.Manu
+{
+    public class ManuModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+    }
+}
---
+++ StudyOfPrism-Menu/StudyApp.Manu/Views/Menu.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace StudyApp.Manu.Views
+{
+    /// <summary>
+    /// Interaction logic for Menu.xaml
+    /// </summary>
+    internal partial class Menu : System.Windows.Controls.Menu
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+    }
+}
