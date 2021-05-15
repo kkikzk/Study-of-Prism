bc2fea60ab8408af63d68d11eb6d046fe521f313
KAKEI Kazuki <kkikzk@gmail.com>
2021-05-15 17:31:17 +0900

    ソリューションとプロジェクトを追加

---
+++ StudyOfPrism-Menu/StudyApp/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Prism.Ioc;
+using StudyApp.Views;
+using System.Windows;
+
+namespace StudyApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+    }
+}
