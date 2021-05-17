84a2c9203100fafca1e72334d7332c8ebed4ff95
KAKEI Kazuki <kkikzk@gmail.com>
2021-05-17 13:25:02 +0900

    WpfDockingのサンプルプロジェクトを追加

---
+++ StudyOfPrism-WpfDocking/StudyOfPrism-WpfDocking/Views/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace StudyOfPrism_WpfDocking.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
